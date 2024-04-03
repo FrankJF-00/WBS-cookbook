466bbc1195c0e8e676c46c7ffc46d840bf7e3740
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-03 18:48:47 +0200

    iframe adjust

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const gallery = document.getElementsByClassName("gallery")[0];
 
 recipeChoice.addEventListener("change", () => {
   gallery.style = "display: none!important;";
+  //   iframe.style.display = "flex";
   iframe.src = recipeChoice.value;
 });
 
 iframe.onload = function () {
-  frame.style.height = frame.contentWindow.document.body.scrollHeight + "px";
-
-  frame.style.width = frame.contentWindow.document.body.scrollWidth + "px";
+  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  iframe.style.width = iframe.contentWindow.document.body.scrollWidth + "px";
+  //   iframe.style.padding = "2rem;";
 };
