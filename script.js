8d8ab78eb9f761d3b4d507e2dcd2db33c1c7f406
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-03 17:23:39 +0200

    added js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const recipeChoice = document.getElementById("recipe-choice");
 const iframe = document.getElementById("iframe");
-console.log(recipeChoice);
+const gallery = document.getElementsByClassName("gallery")[0];
+
+recipeChoice.addEventListener("change", () => {
+  gallery.style = "display: none!important;";
+  iframe.src = recipeChoice.value;
+});
